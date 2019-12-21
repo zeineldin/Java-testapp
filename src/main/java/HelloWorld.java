a9427161a79ed3902fe9f90b3abf346972a206ed
Mohamed <mohamedzeineldin@Mohameds-MBP.fritz.box>
2019-12-21 13:07:31 +0100

    new

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -9,6 +9,6 @@ public class HelloWorld implements SparkApplication {
 
 	@Override
 	public void init() {
-		get("/", (req, res) -> " DevOpsArea Java Sample Project 12");
+		get("/", (req, res) -> "  Java Sample Project 12");
 	}
 }

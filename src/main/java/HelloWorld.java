ef54fbff9662acfa402e447ce7290171d1057c4c
Mohamed Zeineldin <eng.mohamed.zeineldin@gmail.com>
2020-05-16 16:27:46 +0200

    Update HelloWorld.java

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -9,6 +9,6 @@ public class HelloWorld implements SparkApplication {
 
 	@Override
 	public void init() {
-		get("/", (req, res) -> " DevOpsArea 123456789");
+		get("/", (req, res) -> " RAmadan Kareem ");
 	}
 }

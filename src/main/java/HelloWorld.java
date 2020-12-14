522e8f00e74e9d2564280e9cf9093adfe1f83347
Mohamed Zeineldin <eng.mohamed.zeineldin@gmail.com>
2020-12-14 18:25:33 +0100

    Update HelloWorld.java

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -9,6 +9,6 @@ public class HelloWorld implements SparkApplication {
 
 	@Override
 	public void init() {
-		get("/", (req, res) -> "Hello Ansible 2 ");
+		get("/", (req, res) -> "Hello Moustafa ");
 	}
 }

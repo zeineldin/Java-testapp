0494775fa885f0844732552eb5eb71db417b7786
Mohamed Zeineldin <eng.mohamed.zeineldin@gmail.com>
2023-12-26 19:09:11 +0100

    Update HelloWorld.java

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -9,6 +9,6 @@ public class HelloWorld implements SparkApplication {
 
 	@Override
 	public void init() {
-		get("/", (req, res) -> "DevOps Area is comming  26.12.2023!!!");
+		get("/", (req, res) -> "Final Project 26.12.2023!!!");
 	}
 }
